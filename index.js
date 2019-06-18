2196597695b0c47d40c4f7cf9e91c439837ae4a1
ilima <ignacio.lima@anima.edu.uy>
2019-06-18 10:01:38 -0300

    deleted all files

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-app.use(express.urlencoded());
-app.listen(80);
-
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/routes/mainPage/index.html');
-});
