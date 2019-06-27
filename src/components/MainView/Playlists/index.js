9b60ea6590d3a7467771f3f5af12f46adda35fc8
ilima <ignacio.lima@anima.edu.uy>
2019-06-27 10:43:42 -0300

    changes

--- src/components/MainView/Playlists/index.js
+++ src/components/MainView/Playlists/index.js
@@ -2,12 +2,39 @@ import React from 'react';
 import './styles.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
+import song1 from './../../../assets/song2.jpg';
 
 class Playlists extends React.Component {
     render() {
       return (
         <div className="container-fluid" style={{ padding: 0, textAlign: 'center', paddingTop: '5%', backgroundColor: '#352b2b' }}>
             <h1 className="" style={{ color: 'white' }}>Mis playlists</h1>
+            <div style={{ width: '100%', marginTop: '5%', paddingLeft: '5%', paddingRight: '5%', display: 'flex', justifyContent: 'space-between' }}>
+				<div class="" style={{ width: '20%' }}>
+					<img src={song1} class="card-img-top" alt="..." />
+					<div class="" style={{ backgroundColor: 'white', borderBottomLeftRadius: 5, borderBottomRightRadius: 5 }}>
+						<p class="">Cumbias</p>
+					</div>
+				</div>
+				<div class="" style={{ width: '20%' }}>
+					<img src={song1} class="card-img-top" alt="..." />
+					<div class="" style={{ backgroundColor: 'white', borderBottomLeftRadius: 5, borderBottomRightRadius: 5 }}>
+						<p class="">Rock que me gusta</p>
+					</div>
+				</div>
+				<div class="" style={{ width: '20%' }}>
+					<img src={song1} class="card-img-top" alt="..." />
+					<div class="" style={{ backgroundColor: 'white', borderBottomLeftRadius: 5, borderBottomRightRadius: 5 }}>
+						<p class="">Salsa para mover</p>
+					</div>
+				</div>
+				<div class="" style={{ width: '20%' }}>
+					<img src={song1} class="card-img-top" alt="..." />
+					<div class="" style={{ backgroundColor: 'white', borderBottomLeftRadius: 5, borderBottomRightRadius: 5 }}>
+						<p class="">Bachatita</p>
+					</div>
+				</div>
+			</div>
         </div>
       );
     }
--- src/components/MainView/Sidebar/index.js
+++ src/components/MainView/Sidebar/index.js
@@ -3,18 +3,19 @@ import './styles.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import logo from './../../../assets/logo.png';
+import profileImage from './../../../assets/profileImage.png';
 
 class Sidebar extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-
+            modalContent: false,
+            isLoggedIn: true,
         };
     }
 
     render() {
-        console.log(this.props.cambiarVista);
         return (
             <div className="container-fluid" style={{ padding: 0 }}>
                 <div class="sidenav">
@@ -25,22 +26,67 @@ class Sidebar extends React.Component {
                         <button className="sideButtons" onClick={() => { this.props.cambiarVista("Playlists") }}>Mis playlist</button>
                     </div>
                     <div className="bottom-box d-flex flex-column">
-                        <button type="button" class="btn btn-light d-block" style={{ margin: 0, borderRadius: 20 }}  data-toggle="modal" data-target="#login-register-modal">Iniciar sesión</button>
-                        <button type="button" class="btn btn-light d-block" style={{ marginTop: 16, borderRadius: 20 }}  data-toggle="modal" data-target="#login-register-modal">Registrarse</button>
+                        {this.state.isLoggedIn ?
+                            <div style={{ textAlign: 'center', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }} >
+                                <img src={profileImage} alt="profile" style={{ borderRadius: 100, width: '40%' }} />
+                                <button className="profileName" style={{ fontSize: 14 }}>Cristian Mello</button>
+                            </div>
+                        :
+                            <div className="d-flex flex-column">
+                                <button type="button" class="btn btn-light d-block" style={{ margin: 0, borderRadius: 20 }}  data-toggle="modal" data-target="#login-register-modal" onClick={ () => { this.setState({ modalContent: false }) } }>Iniciar sesión</button>
+                                <button type="button" class="btn btn-light d-block" style={{ marginTop: 16, borderRadius: 20 }}  data-toggle="modal" data-target="#login-register-modal" onClick={ () => { this.setState({ modalContent: true }) } }>Registrarse</button>
+                            </div>
+                        }
                     </div>
                 </div>
                 <div class="modal fade" id="login-register-modal" tabindex="-1" role="dialog" aria-labelledby="login-register-modal" aria-hidden="true">
                     <div class="modal-dialog modal-dialog-centered" role="document">
                         <div class="modal-content">
                             <div class="modal-header" style={{ justifyContent: 'center' }}>
-                                <h2>Login</h2>
+                                {this.state.modalContent ?
+                                    <h2>Registrarse</h2>
+                                :
+                                    <h2>Iniciar sesión</h2>
+                                }
                             </div>
                             <div class="modal-body">
-                                ...
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Save changes</button>
+                                {this.state.modalContent ?
+                                    <div style={{ paddingTop: '5%', paddingBottom: '5%' }}>
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1">Nombre</label>
+                                            <input type="naim" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nombre" />
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1">Correo electrónico</label>
+                                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Correo electrónico" />
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="Password">Contraseña</label>
+                                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Contraseña" />
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="confirmPassword">Confirmar contraseña</label>
+                                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Confirmar contraseña" />
+                                        </div>
+                                        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10%' }}>
+                                            <button type="submit" class="btn btn-primary">Registrarse</button>
+                                        </div>
+                                    </div>
+                                :
+                                    <div style={{ paddingTop: '5%', paddingBottom: '5%' }}>
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1">Correo electrónico</label>
+                                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Correo electrónico" />
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="exampleInputPassword1">Contraseña</label>
+                                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Contraseña" />
+                                        </div>
+                                        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10%' }}>
+                                            <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#login-register-modal" onClick={ () => { this.setState({ isLoggedIn: true }) } }>Ingresar</button>
+                                        </div>
+                                    </div>
+                                }
                             </div>
                         </div>
                     </div>
